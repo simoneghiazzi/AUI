29affdf816ac141cf2cbdb8889a2bb11d686f361
Louis <louismarie.lecoeur@gmail.com>
2020-12-15 00:05:45 +0100

    Changes on Script+Elements of the Quizz
    - Small adjustments on the elements of the scene (resize...) + use of different panels (questions, intermediate results, final results,...)
    - Script = 4 questions in a row, adding of a temporisation (5 seconds) between the questions, display the right panel at the right time
    /!\ The Quizz game works for only Team1 for the moment !

--- Assets/Scripts/LouisScripts/QuizzManager.cs
+++ Assets/Scripts/LouisScripts/QuizzManager.cs
@@ -6,23 +6,70 @@ using UnityEngine.SceneManagement;
 
 public class QuizzManager : MonoBehaviour
 {
+    /* VARIABLES DECLARATION AND INITIALIZATION*/
+
     public List<QuestionAndAnswers> QnA;
     public GameObject[] options;
-    public int currentQuestion;
+    public int currentQuestion = 0;
+
+    public GameObject QuestionsPanel;
+    public GameObject PartialResultsPanel;
+    public GameObject FinalResultsPanel;
+    public GameObject UIElements;
 
     public Text QuestionTxt;
+    public Text ScoreTxt;
 
+    int totalQuestions = 0; //total number of questions
+    public int scoreTeam1 = 0;
+
+
+    /* METHODS */
+
+    //Beginning of the Quizz game = set the question panel + UI elements
+    //                              (= background, Leo character, ...)
     private void Start()
     {
+        totalQuestions = QnA.Count;
+        PartialResultsPanel.SetActive(false);
+        FinalResultsPanel.SetActive(false);
         generateQuestion();
     }
 
+    //End of the game = display the final results
+    void GameOver()
+    {
+        QuestionsPanel.SetActive(false);
+        PartialResultsPanel.SetActive(false);
+        FinalResultsPanel.SetActive(true);
+        ScoreTxt.text = (scoreTeam1).ToString();
+    }
+
+    //When an answer is correct
     public void correct()
     {
+        scoreTeam1 += 1;
         QnA.RemoveAt(currentQuestion);
+        IntermediateResults();
+        StartCoroutine(waitForNext());
+    }
+
+    //When an answer is wrong
+    public void wrong()
+    {
+        QnA.RemoveAt(currentQuestion);
+        IntermediateResults();
+        StartCoroutine(waitForNext());
+    }
+
+    //Wait for next question
+    IEnumerator waitForNext()
+    {
+        yield return new WaitForSeconds(5);
         generateQuestion();
     }
 
+    //Method used to manage the answers from the Unity Inspector
     void SetAnswers()
     {
         for(int i = 0; i < options.Length; i++)
@@ -37,19 +84,32 @@ public class QuizzManager : MonoBehaviour
         }
     }
 
+    //Method used to generate a question from the Unity Inspector
     void generateQuestion()
     {
-        if(QnA.Count > 0)
+        if(QnA.Count > 0) //when there are questions available
         {
           currentQuestion = Random.Range(0, QnA.Count);
 
           QuestionTxt.text = QnA[currentQuestion].Question;
           SetAnswers();
+          QuestionsPanel.SetActive(true);
+          PartialResultsPanel.SetActive(false);
         }
-        else
+        else //when there are no more questions
         {
           Debug.Log("Out of Questions");
+          GameOver();
         }
 
     }
+
+    //After each question = display the intermediate results
+    void IntermediateResults()
+    {
+        ScoreTxt.text = (scoreTeam1).ToString();
+        QuestionsPanel.SetActive(false);
+        PartialResultsPanel.SetActive(true);
+    }
+
 }
