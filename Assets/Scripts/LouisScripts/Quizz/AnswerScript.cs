46117de82d6776841f99b114cb904c9377b5dc6c
Louis <57670011+SVVAYZ@users.noreply.github.com>
2021-01-09 23:49:56 +0100

    Step scrolling fixed + adding quiz colliders

--- Assets/Scripts/LouisScripts/Quizz/AnswerScript.cs
+++ Assets/Scripts/LouisScripts/Quizz/AnswerScript.cs
@@ -7,13 +7,38 @@ public class AnswerScript : MonoBehaviour
 {
     public bool isCorrect = false;
     public GameObject FloorQuizzManager;
+    Collider AnswerBubble;
 
     public void Start()
     {
 
     }
 
-    public void AnswersManager() // method called when the player clicks on the button
+    public void OnTriggerEnter(Collider AnswerBubble) // method called when the player collides with the button
+    {
+        Debug.Log("Answer selected, wait 3 seconds for validation");
+
+        StartCoroutine(waitForValidation(3)); // Waiting 3 seconds on the button to validate the button
+
+    }
+
+
+    IEnumerator waitForValidation(int time)
+    {
+      yield return new WaitForSeconds(time);
+
+      Debug.Log("Answer validated");
+      AnswersManager();
+    }
+
+
+    public void OnTriggerExit(Collider AnswerBubble) // method called when the player stop stepping on the answer bubble
+    {
+      Debug.Log("Answer deselected");
+    }
+
+
+    public void AnswersManager() // method called when the player collides with the answer bubble, and waited 3 seconds on the same answer bubble
     {
         if(isCorrect == true)
         {
@@ -26,6 +51,7 @@ public class AnswerScript : MonoBehaviour
             // use the id of the team as the index of the score array
             // alreay answered array = INITIALIZATION false, and everytime an answer is given we put true for the corresponding team
         }
+
 /*
         if( alreadyAnswered == false )
         {
@@ -33,6 +59,7 @@ public class AnswerScript : MonoBehaviour
             // Code for Time is out
         }
 */
+
         if(isCorrect == false)
         {
             Debug.Log("Wrong Answer");
@@ -42,4 +69,5 @@ public class AnswerScript : MonoBehaviour
         }
 
     }
+
 }
