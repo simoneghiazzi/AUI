944d15e92670fc1cd7d754b763ccd80ed76b0578
Louis <57670011+SVVAYZ@users.noreply.github.com>
2021-02-01 12:44:32 +0100

    Menu Sprites placed

---
+++ Assets/Resources/Scripts/LouisScripts/Menu/MenuText.cs
@@ -0,0 +1,13 @@
+﻿[System.Serializable]
+
+public class MenuText
+{
+  public string[] FrontMuseumText;
+  public string[] LeoPresentationText;
+  public string[] MuseumEntranceText;
+  public string[] ModalitiesText;
+  public string[] SingolaText;
+  public string[] MultiplaText;
+  public string[] SubjectText;
+  public string[] DaVinciTopicsText;
+}
